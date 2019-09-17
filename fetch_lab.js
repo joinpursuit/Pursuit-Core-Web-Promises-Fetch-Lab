fe62036c6f8ad4bdbcbfaf06a97c33d1aceeca5e
Johanne <johanneenama@gmail.com>
2019-09-17 19:22:45 -0400

    updated

--- fetch_lab.js
+++ fetch_lab.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     let button = document.querySelector("button");
-    button.addEventListener("click", randomlyLoadedJokes )
+    let jokeList = document.querySelector("li")
+    button.addEventListener("click", randomlyLoadedJokes)
+    
 })
 
 const randomlyLoadedJokes = () => {
@@ -9,33 +11,47 @@ const randomlyLoadedJokes = () => {
     .then(response => {
         return response.json()
     })  
-    .then(data => {
-        console.log(data)
-        displayJokes(data.setup)
+    .then(jokes => {
+         console.log('jokessssss', jokes)
+         
+        displayJokes(jokes)    
+    })
+    // .then(answer => {
         
+    // })
+    .catch(err => {
+        console.log("err:" , err)
     })
    
 } 
 
 const displayJokes = (jokes) => {
-    for (let joke of jokes)
-    let ul = document.querySelector("#list-jokes")
-    let firstList = document.createElement("li")
-    // let prevJoke = document.querySelector("li")
-        firstList.innerText = joke.setup
-    // if (prevJoke === null) {
-    //     let ul = document.querySelector("#list-jokes")
-    //     let firstList = document.createElement("li")
-    //     firstList.innerText = joke
-        ul.appendChild(firstList)
-    //     console.log(firstList)
-    // } else  {
-    //     let newJoke = document.createElement("li")
-    //     newJoke.innerText = joke
-    //     prevJoke.parentNode.replaceChild(newJoke, prevJoke)
-    // }
+    let oldUl = document.querySelector("ul")
+    let newUl = document.createElement("ul")
+    for (let joke of jokes) {
+        let jokeList = document.createElement("li")
+        jokeList.innerText = joke.setup
+        let p = document.createElement("p")
+        p.innerText = joke.punchline
+        p.classList.add('invisible')
+        jokeList.addEventListener("click", togglePunchline)
+        console.log(jokeList.innerText)
+        newUl.appendChild(jokeList)
+        newUl.appendChild(p)
+        console.log(jokeList.nextElementSibling);
+    }  
+        oldUl.parentNode.replaceChild(newUl, oldUl)
 }
 
-// const displayPunchline = (answer) => {
+const togglePunchline = (e) => {
+    console.log("event", e.target)
+    console.log("next sibling", e.target.nextElementSibling)
 
-// }
+    // let p = document.querySelector("p")
+    // console.log("p", p)
+    e.target.nextElementSibling.classList.toggle("invisible")
+    // console.log("class", p.classList )
+    
+}
+
+
