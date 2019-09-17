534aff7454e7e01f62412366eb00ac81b96dd730
gsanchez13 <gisellesanchez@pursuit.org>
2019-09-16 20:20:13 -0400

    added event listeners to divs with punchlines

--- fetchlab.js
+++ fetchlab.js
@@ -1,31 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("runningggg")
-    generateJokes()
-    // let opened = false 
-})
-const generateJokes = () => {
-    let list = document.querySelector("ul");
-    fetch("https://api.chucknorris.io/jokes/random")
+document.addEventListener("DOMContentLoaded", () => {})
+
+let jokeObject = {}; // holds value of joke object
+
+const generateJoke = () => {
+    fetch("https://official-joke-api.appspot.com/jokes/random")
         .then((jokeReponse) => {
-            console.log(`JOKE RESPONSE: ${jokeReponse}`)
             return jokeReponse.json();
         })
         .then((jokeObj) => {
-            console.log(`JOKE OBJECT: ${jokeObj}`);
-            newListItem(jokeObj.value)
+            jokeObject = jokeObj
+            newListItem(jokeObj.setup)
+            return(jokeObj)
         })
 }
+generateJoke(); // synchronous, generates a joke and sets my empty object to a Joke Object
+
 const newListItem = (newItemContent) => {
-    // let list = document.querySelector("ul");
     let newItem = document.querySelector("div")
     if(!newItem){
         newItem = document.createElement("div");
-        console.log(newItem)
         newItem.innerText = newItemContent;
         document.body.appendChild(newItem)
+
+        newItem.addEventListener("click", revealAnswer)
 }   else {
         let newJoke = document.createElement("div");
         newJoke.innerText = newItemContent;
         newItem = document.body.replaceChild(newJoke, newItem)
+        newJoke.addEventListener("click", revealAnswer)
+}}
+
+const getPunchline = () => {
+    let punchline = jokeObject.punchline;
+    return punchline
 }
+const revealAnswer = () => {
+    let answer = getPunchline();
+    document.querySelector("div").innerText += answer;
+}
+const hideAnswer = () => {
+    
 }
