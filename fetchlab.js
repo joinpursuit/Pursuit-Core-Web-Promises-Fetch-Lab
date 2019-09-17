2ea218417b4e7f94b6a67d1eee34d95594b6ebd7
gsanchez13 <gisellesanchez@pursuit.org>
2019-09-16 20:37:38 -0400

    div reveals and hides answer

--- fetchlab.js
+++ fetchlab.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {})
 
 let jokeObject = {}; // holds value of joke object
-
 const generateJoke = () => {
     fetch("https://official-joke-api.appspot.com/jokes/random")
         .then((jokeReponse) => {
@@ -14,14 +13,13 @@ const generateJoke = () => {
         })
 }
 generateJoke(); // synchronous, generates a joke and sets my empty object to a Joke Object
-
 const newListItem = (newItemContent) => {
     let newItem = document.querySelector("div")
     if(!newItem){
         newItem = document.createElement("div");
         newItem.innerText = newItemContent;
         document.body.appendChild(newItem)
-
+        newItem.class = "closed"
         newItem.addEventListener("click", revealAnswer)
 }   else {
         let newJoke = document.createElement("div");
@@ -29,15 +27,17 @@ const newListItem = (newItemContent) => {
         newItem = document.body.replaceChild(newJoke, newItem)
         newJoke.addEventListener("click", revealAnswer)
 }}
-
 const getPunchline = () => {
     let punchline = jokeObject.punchline;
     return punchline
 }
 const revealAnswer = () => {
     let answer = getPunchline();
+    if(document.querySelector("div").class === "closed"){
     document.querySelector("div").innerText += answer;
-}
-const hideAnswer = () => {
-    
+    document.querySelector("div").class = "opened"
+    } else {
+        document.querySelector("div").innerText = jokeObject.setup;
+        document.querySelector("div").class = "closed"
+    }
 }
