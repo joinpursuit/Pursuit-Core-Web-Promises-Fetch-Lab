f33de286aad7d47f22f6601af2a69733a7f0e6ca
bsihame <sihamebazi@pursuit.org>
2019-09-18 19:58:25 -0400

    commit for twitter

--- fetchLab.js
+++ fetchLab.js
@@ -1,44 +1,89 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Dom Loaded");
-  fetchRandomJoke();
+
 
   let button = document.querySelector("button");
+  let parentList = document.createElement("ul");
+  document.body.appendChild(parentList);
+
   console.log("button grabbed");
-  button.addEventListener("click", fetchRandomJoke);
   console.log("domCont listner is done");
-});
 
-const fetchRandomJoke = () => {
-  console.log(" fetchJoke started");
-  fetch("https://official-joke-api.appspot.com/random_ten")
-    .then(response => {
-      return response.json();
-      console.log(response.json);
-    })
-    .then(jokes => {
-      console.log(jokes)
-      displayJokes(jokes)
+  const fetchRandomJoke = () => {
+    console.log("fetchJoke started");
+    fetch("https://fsw62-jokes-api.herokuapp.com/jokes/random/3")
+      .then(response => {
+        return response.json();
+        console.log(response.json);
+      })
+      .then(jokes => {
+        console.log(jokes);
+        displayJokes(jokes);
+      })
+      .catch(err => {
+        console.log("err:", err);
+      });
+    console.log("fetchRandomJock");
+  };
 
-      // const { randomJoke, randomTen } = require("./handler");
-      // console.log("randomJoke", randomJoke());
-      // console.log("randomTen", randomTen());
-    })
-    .catch(err => {
-      console.log("err:", err)
-    })
-    console.log('fetchRandomJock')
-}
-  const displayJokes =(jokes)=> {
-    let parentList = document.createElement("ul")
-    for(let joke of jokes ){
-      let jokeLi = document.createElement("li")
-      jokeLi.innerText = joke.setup
-      parentList.appendChild(jokeLi)
-      console.log(joke)
+  const showPunchline = (e) =>{
+    console.log("e target", e.target.nextSibling)
+    let punchline = e.target.nextSibling
+    if(punchline.style.display === "none") {
+      punchline.style.display = "block"
+    } else {
+      punchline.style.display = "none"
+
     }
-    document.body.appendChild(parentList)
-    console.log(parentList)
-    // firstJoke.innerText= joke
-    // parentList.appendChild(firstJoke) 
+    // console.log('joke was clicked') 
+  };
+
+  const displayJokes = (jokes) => {
+    console.log("child nodes?", parentList.hasChildNodes())
+    if (!parentList.hasChildNodes()) {
+      console.log("running first")
+      for (let joke of jokes) {
+        let jokeLi = document.createElement("li");
+        let setup = document.createElement("p")
+        let punchline = document.createElement("p");
+        punchline.style.display = "none"
+        setup.innerText = joke.setup;
+        punchline.innerText = joke.punchline
+
+        jokeLi.appendChild(setup)
+        jokeLi.appendChild(punchline)
+
+        parentList.appendChild(jokeLi);
+
+        jokeLi.addEventListener("click", showPunchline)
+        console.log(setup);
+      }
+    } else {
+      console.log("running second")
+
+      for (let i = 0; i < jokes.length; i++) {
+        console.log("jokeLi", parentList)
+        // console.log("joke LIIII", jokeLi)
+        let newJoke = document.createElement("li");
+        let newPunchline = document.createElement("p")
+        let newSetup = document.createElement("p")
+        newJoke.appendChild(newSetup)
+        newJoke.appendChild(newPunchline)
+        // newJoke.innerText = jokes[i].setup;
+        newPunchline.innerText = jokes[i].punchline;
+        newPunchline.style.display = "none"
+        newSetup.innerText = jokes[i].setup;
+        let li = parentList.childNodes[i]
+        console.log("parent child node", li.childNodes[1])
+        newJoke.addEventListener("click", showPunchline)
+     
+        parentList.replaceChild(newJoke, li)
+        console.log(jokes[i]);
+      }
+
+    }
+  button.addEventListener("click", fetchRandomJoke);
   }
-console.log("this is a js")
+  fetchRandomJoke();
+});
