b1eceb49747895ecb42160d50bc47e72aa40eaee
rafidhossain1995 <rafidhossain@pursuit.org>
2019-11-26 21:29:04 -0500

    Finished Part 1

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
     let joke1 = document.querySelector("#jokes")
-
-    fetch("https://official-joke-api.appspot.com/random_joke").then(res=>{
+    let joke2 = document.querySelector("#punchline")
+    fetch("https://cors-anywhere.herokuapp.com/https://official-joke-api.appspot.com/random_joke").then(res=>{
         if(!res.ok){
             throw Error(res.statusText="its Not Ok")
         }
-        
         return res.json()
     }).then(res =>{
         joke1.innerText = res.setup
-    }).catch(err =>{
-        
+        joke2.addEventListener("click", () => {
+        if(joke2.innerText !== "Joke Card"){
+            return joke2.innerText = "Joke Card"
+        } else{
+        joke2.innerText = res.punchline
+        }
     })
-
+    }).catch(err =>{
+  })
 })
+let refresh = document.querySelector("#refresh")
+function refreshPage(){
+    window.location.reload();   
+}
 
-//     let joke2 = document.querySelector("#punchline")
-//     joke2.addEventListener("click", () => {
-//     fetch(("https://official-joke-api.appspot.com/random_joke").then(res=>{
-//         if(!res.ok){
-//             throw Error(res.statusText ="it's not ok")
-//         }
-//         debugger
-//         return res.json()
-//     }).then(res =>{
-//         joke2.innerText = res.punchline
-//     }).catch(err =>{
-//         debugger
-//     }))
-// })
