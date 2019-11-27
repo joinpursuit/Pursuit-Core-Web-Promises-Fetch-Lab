f1f6451230dc0c2f295c0af33e06613d3960133c
serami12 <stephanieramirez1@pursuit.org>
2019-11-26 22:11:06 -0500

    fecth setup and punchline

--- index.js
+++ index.js
@@ -8,16 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
             return res.json()
         }).then(res => {
 
-
-            let ul = document.querySelector("ul");
             res.forEach(joke => {
+                let ul = document.querySelector("#list");
+                // let card = createCardFromJoke(joke)
                 let li = document.createElement("li")
                 li.innerText = joke.setup
+                let p = document.createElement("p")
+                p.innerText = joke.punchline
+
                 ul.appendChild(li)
+                li.appendChild(p)
             })
-
-
         })
     }
+    // createCardFromJoke = (joke) => {
+    //     let ul = document.querySelector("#list");
+    //     let newCard = document.createElement("div")
+    //     newCard.className = "card"
+    //     newCard.style = "width:300px"
 
+    //     let jokeHeader = document.createElement("h4")
+    //     jokeHeader.innerText = `${joke.setup}`
+    //     newCard.appendChild(jokeHeader)
+    //     ul.appendChild(newCard)
+    // }
+    getJokes()
 })
