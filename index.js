ecdb86288a41cd189a9989e6c1f79b1629e0ce11
Kevin Wong <kevinwong@pursuit.org>
2019-11-27 01:05:33 -0500

    created button that refreshes page, created a form select in html file, trying to fetch new data from new URL by joke type to repopulate list with jokes by type

--- index.js
+++ index.js
@@ -18,12 +18,40 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             li.innerText = joke.setup
             ul.appendChild(li)
-            let button = document.createElement("button")
-            button.innerHTML = "refresh"
-            
         })
+        let button = document.createElement("button")
+        button.innerHTML = "REFRESH"
+        button.addEventListener("click", ()=> {
+            window.location.reload();
+        })
+        let select = document.querySelector("#selector")
+
         document.body.appendChild(ul)
+        document.body.appendChild(button);
     }).catch(err => {
         debugger
     })
 })
+
+// createSelect = function(){
+//     let selectDiv = document.querySelector("#select");
+//     let form = document.createElement("form");
+//     form.id = "selectForm";
+//     let select = document.createElement("select");
+//     select.name = "joketype"
+//     select.form = "selectForm"
+//     let option1 = document.createElement("option");
+//     option1.value = "general"
+//     option1.innerText = "General";
+//     let option2 = document.createElement("option");
+//     option2.value = "random"
+//     option2.innerText = "Random";
+//     let option3 = document.createElement("option");
+//     option3.value = "programming"
+//     option3.innerText = "Programming";
+//     select.appendChild(option1);
+//     select.appendChild(option2);
+//     select.appendChild(option3);
+//     selectDiv.appendChild(form);
+//     selectDiv.appendChild(select);
+// }
