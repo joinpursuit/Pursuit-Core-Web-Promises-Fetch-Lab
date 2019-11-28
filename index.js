7e29eb477ea7b6c58794dd7e1e41a907e8794f92
bsihame <sihamebazi@pursuit.org>
2019-11-27 19:15:12 -0500

    Lab is not done

--- index.js
+++ index.js
@@ -2,18 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("https://cors-anywhere.herokuapp.com/https://official-joke-api.appspot.com/jokes/ten").then(res => {
     return res.json();
 }).then(jokes => {
-  let ul = document.querySelector('ul');
+  let ul = document.querySelector('#ul');
   jokes.forEach(joke => {
-   let li = document.querySelector('li');
+   let li = document.querySelector('#li');
    li.innerText = joke.li;
    let punchline = document.createElement('li')
    punchline.innerText = joke.punchline;
             punchline.style.display = "none";
-            punchline.id = 'punchline' 
+            punchline.className = 'punchline' 
             let ulPunchline = document.createElement('ul');
             ulPunchline.appendChild(punchline);
             li.appendChild(ulPunchline);
             ul.appendChild(li)
   })
   document.body.appendChild(ul)
+})
+let ul = document.querySelector('#ul');
+ul.addEventListener("click", (e) => {
+  let punchline = e.target.children[0].firstChild;
+        if(punchline.style.display === "none") {
+            punchline.style.display = "block";
+        } else {
+            punchline.style.display = "none";
+        }
+})
 })
